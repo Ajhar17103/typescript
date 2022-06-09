60b38818321c87c9b05f05cc288c61faf1173b24
Ajhar17103 <contact.ajharislam@gmail.com>
2022-06-09 18:34:20 +0600

    dynamic veriable

--- output/script.js
+++ output/script.js
@@ -1,3 +1,18 @@
 "use strict";
 const names = "I'm Azharul";
 console.log(names);
+/* dynamic veriable */
+let a;
+a = "name";
+console.log(a);
+a = 10;
+console.log(a);
+let b = [];
+b.push('azhar', 100, "islam", { age: 25, jobStatus: 'software Developer' });
+console.log(b);
+let c = [];
+c.push({ name: "azharul Islam", Salary: 2500 }, { name: "Nazmus Sakib", Salary: 25000 });
+console.log(c);
+let total;
+let totalSalary = c.reduce((arra, index) => total += arra.Salary, 0);
+console.log(totalSalary);
--- script.ts
+++ script.ts
@@ -1,3 +1,31 @@
 const names= "I'm Azharul";
 
 console.log(names)
+
+
+/* dynamic veriable */
+
+let a:any;
+
+a="name"
+console.log(a)
+
+a=10
+console.log(a)
+
+let b:any=[]
+
+b.push('azhar',100, "islam", {age:25, jobStatus:'software Developer'})
+
+console.log(b)
+
+
+
+let c:any=[]
+c.push({name:"azharul Islam", Salary:2500},{name:"Nazmus Sakib", Salary:25000})
+
+console.log(c)
+let total:number
+let totalSalary=c.reduce((arra:any, index:any)=>total+=arra.Salary,0)
+
+console.log(totalSalary)
